57ed7bf8df6178862cc0ce41799b41fee0e70ebb
Surbhi Mittal <surbhim@iiitd.ac.in>
2019-10-18 19:36:07 +0530

    complete fn for bright images

--- Source/improved_adaptive_gamma_correction.m
+++ Source/improved_adaptive_gamma_correction.m
@@ -25,19 +25,51 @@ figure; imshow(uint8(y)), title('Improved Gamma Correction Transformed Image');
 % paper parameters
 T = 112;           % T is appropriate to be set as about the half of maximum pixel intensity, i.e., 128 for 8-bit images.
 thresh = 0.3;      % threshold used for distinguishing brightness-distorted images from normal ones. set experimentally in consideration of the trade-off between enhancement quality and technical applicability.
-% trunc = 0.5;
-% alpha_dimmed = 0.75;
-% alpha_bright = 0.25;
+trunc = 0.5;
+alpha_dimmed = 0.75;
+alpha_bright = 0.25;
 
 function y = ce_dimmed(x)
-
-    y =0;
-
+    
+    alpha = alpha_dimmed;
+    
+    
 end
 
 function y = ce_bright(x)
 
-    y =0;
+    alpha = alpha_bright;
+    l_max = 255;
+    [M,N]=size(x); % get size of image
+    total_pixels = M*N;
+    
+    x = 255 - x;
+    
+    for i=0:255
+       PDF1(i+1)=sum(sum(x==i))/total_pixels; %hist of input image
+    end
+
+    p_max = max(PDF1);
+    p_min = min(PDF1);
+    
+    %weighting dist fn to smooth primary hist
+    for i=0:255
+        PDF2(i+1) = p_max * (((PDF1(i+1)-p_min)/(p_max-p_min))^alpha);
+    end
+
+    %normalize prob
+    PDF3 = PDF2/sum(PDF2);
+   
+    PDF = PDF3;
+    y=x; 
+    for i=0:255
+       I = find(x==i); %index of pixels in input image with value ‘i’
+       CDF = sum(PDF(1:i));
+       gamma = 1 - CDF;
+       y(I) = round(l_max*((i/l_max)^gamma)); 
+    end
+    
+    y = round(255 - y);
 
 end
 
@@ -60,27 +92,3 @@ function y = improved_gamma_transform(x)
     end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-%     for i=0:255
-%        PDF1(i+1)=sum(sum(x==i))/total_pixels; %hist of input image
-%     end
-% 
-%     p_max = max(PDF1);
-%     p_min = min(PDF1);
-%     
-%     %weighting dist fn to smooth primary hist
-%     for i=0:255
-%         PDF2(i+1) = p_max * (((PDF1(i+1)-p_min)/(p_max-p_min))^alpha);
-%     end
-%     
-%     %normalize prob
-%     PDF3 = PDF2/sum(PDF2);
-%    
-%     PDF = PDF3;
-%     y=x; 
-%     for i=0:255
-%        I = find(x==i); %index of pixels in input image with value ‘i’
-%        CDF = sum(PDF(1:i));
-%        gamma = 1 - CDF;
-%        y(I) = round(l_max*((i/l_max)^gamma)); 
-%     end
