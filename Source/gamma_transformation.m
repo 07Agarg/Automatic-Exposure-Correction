75bac54513126cdc89b0295ba437771686015b0b
Surbhi Mittal <surbhim@iiitd.ac.in>
2019-11-06 17:32:35 +0530

    new_gamma_transform

--- Source/gamma_transformation.m
+++ Source/gamma_transformation.m
@@ -1,11 +1,14 @@
+%Adaptive Gamma Transformation: Prior Works
+%source: https://arxiv.org/ftp/arxiv/papers/1709/1709.04427.pdf
+
 clc;
 clear all;
 close all;
 
 D = '../Dataset/Part A/';
 %D = '../Dataset/Part B/';
-S = fullfile(pwd, D, 'IMG_1.png');
-% S = fullfile(pwd, D, 'IMG_2.png');
+% S = fullfile(pwd, D, 'IMG_1.png');
+S = fullfile(pwd, D, 'IMG_2.png');
 % S = fullfile(pwd, D, 'IMG_3.png');
 % S = fullfile(pwd, D, 'IMG_4.png');
 % S = fullfile(pwd, D, 'IMG_5.png');
@@ -13,27 +16,45 @@ S = fullfile(pwd, D, 'IMG_1.png');
 % S = fullfile(pwd, D, 'IMG_7.png');
 % S = fullfile(pwd, D, 'IMG_8.png');
 % S = fullfile(pwd, D, 'IMG_9.png');
-im = imread(S);
-figure; imshow(im)
 
-%gray_i = grb2gray(im);
-im_gamma = imadjust(im, [], [], 1.5);
-figure, imshow(im_gamma)
+x=double(imread(S)); % input image
+xr = x(:,:,1);
+xg = x(:,:,2);
+xb = x(:,:,3);
+imshow(uint8(x)), title('Original Image');
 
-LAB = rgb2lab(im_gamma);
-L = LAB(:,:,1)/100;
-L = adapthisteq(L,'NumTiles',[720 50],'ClipLimit',0.005);
-LAB(:,:,1) = L*100;
-J_720 = lab2rgb(LAB);
-figure;imshow(J_720)
-%imshowpair(im,J,'montage')
-title('Contrast Enhanced Image-(720 50)')
+gamma = 1.5;
+yr = adapt_gamma_transform(xr, gamma); 
+yg = adapt_gamma_transform(xg, gamma);
+yb = adapt_gamma_transform(xb, gamma);
 
-LAB = rgb2lab(im_gamma);
-L = LAB(:,:,1)/100;
-L = adapthisteq(L,'NumTiles',[250 50],'ClipLimit',0.005);
-LAB(:,:,1) = L*100;
-J_250 = lab2rgb(LAB);
-%figure;imshow(J_250)
-%imshowpair(im,J,'montage')
-% title('Contrast Enhanced Image-(250 50)')
+y = cat(3,uint8(yr),uint8(yg),uint8(yb));
+% figure; imshow(uint8(y)), title('Adaptive Gamma Transformed Image')
+
+brisque_orig_img = round(brisque(x), 4);
+brisque_adaptgamma = round(brisque(y), 4);
+
+niqe_orig_img = round(niqe(x), 4);
+niqe_adaptgamma = round(niqe(y), 4);
+
+piqe_orig_img = round(piqe(x), 4);
+piqe_adaptgamma = round(piqe(y), 4);
+
+
+function y = adapt_gamma_transform(x, gamma)
+    l_max = 255;
+    [M,N]=size(x); % get size of image
+    total_pixels = M*N;
+    
+    for i=0:255
+       PDF(i+1)=sum(sum(x==i))/total_pixels; %hist of input image
+    end
+
+    y=x; 
+    for i=0:255
+       I = find(x==i); %index of pixels in input image with value ‘i’
+       CDF = sum(PDF(1:i));
+       % gamma = 1 - CDF;
+       y(I) = round(l_max*((i/l_max)^gamma)); 
+    end
+end
