0f29fa881dbd52bedf80c3ef0a5566ec9e13961a
Surbhi Mittal <surbhim@iiitd.ac.in>
2019-11-30 06:51:39 +0530

    add files

--- Source/adaptive_nonlinear_stretching.m
+++ Source/adaptive_nonlinear_stretching.m
@@ -9,8 +9,9 @@ im = imread(S); % Read the Image
 hsv_x = rgb2hsv(im);
 
 i = hsv_x(:,:,3);
-i255 = uint8(hsv_x(:,:,3)*255);
-hsv_y = adapt_nonlinear_stretch(i, i255);
+% i255 = uint8(i*255);
+
+hsv_y = adapt_nonlinear_stretch(i);
 %i_new = double(hsv_y)/255.0;
 hsv_x(:,:,3) = hsv_y;
 y =  hsv2rgb(hsv_x);
@@ -22,79 +23,69 @@ brisque_orig_img = round(brisque(im), 4);
 brisque_adaptnonlinear = round(brisque(y), 4);
 
 function threshold = otsu_threshold(I)
-    n=imhist(I); % Compute the histogram
-    N=sum(n); % sum the values of all the histogram values
-    max=0; %initialize maximum to zero
-    %%================================================================================================
-    for i=1:256
-        P(i)=n(i)/N; %Computing the probability of each intensity level
-    end
-    %%================================================================================================
-    for T=2:255      % step through all thresholds from 2 to 255
-        w0=sum(P(1:T)); % Probability of class 1 (separated by threshold)
-        w1=sum(P(T+1:256)); %probability of class2 (separated by threshold)
-        u0=dot([0:T-1],P(1:T))/w0; % class mean u0
-        u1=dot([T:255],P(T+1:256))/w1; % class mean u1
-        sigma=w0*w1*((u1-u0)^2); % compute sigma i.e variance(between class)
-        if sigma>max % compare sigma with maximum 
-            max=sigma; % update the value of max i.e max=sigma
-            threshold=T-1; % desired threshold corresponds to maximum variance of between class
-        end
+    [counts,x] = imhist(I);
+    threshold = otsuthresh(counts);
+end
+
+
+function [px, xpx] = ret_px_xpx(PDF, low, high)
+    px = sum(PDF(low+1:high));
+    xpx = 0;
+    for i=low:high
+       xpx = xpx + double(i/255) * PDF(i+1);
     end
 end
 
-function y = summationXPX(x, low, high)
-    %Calculate px
+
+function hist = calc_hist(x)
     [M,N]=size(x); % get size of image
-    total_pixels = M*N;  
-    for i=0:255
-       PDF(i+1)=sum(sum(x==i))/total_pixels; %hist of input image
-    end
-    y = x;
-    for i=low:high
-       I=find(x==i); %index of pixels in input image with value ‘i’
-       xpx = x(I, 0) * PDF(low:i);
-       CDF = sum(xpx);
-       y(I)=round(CDF*255); %(L-1)*CDF
-    end
+    total_pixels = M*N;
     
-end
-
-function y = summationPx(x, low, high)
-    [M,N]=size(x); % get size of image
-    total_pixels = M*N;  
     for i=0:255
        PDF(i+1)=sum(sum(x==i))/total_pixels; %hist of input image
     end
-    y = x;
-    for i=low:high
-       I=find(x==i); %index of pixels in input image with value ‘i’
-       CDF = sum(PDF(low:i));
-       y(I)=round(CDF*255); %(L-1)*CDF
-    end
+    hist = PDF;
 end
 
-function kbar = calc_kbar(x)
+function kbar = calc_kbar(x, b)
     ybar = 0.5;                       %%% Given in paper
-    b = otsu_threshold(x);            %%% In range 0- 255
-    a1 = summationXPX(x, b, 255);
-    b1 = summationPx(x, b, 255);
-    c1 = summationXPX(x, 0, b);
-    d1 = summationPx(x, b, 255);
-    e1 = summationXPX(x, 0, 255);
-    num = (255 - b) * ybar - a1 + b * b1;
-    den = c1 + b * (d1) - b * e1;
     
-    kbar = b * (num/den)
+    x255 = uint8(x*255);
+    x_hist = calc_hist(x255);
+    b_255 = uint8(b*255);
+    
+    [b1, a1] = ret_px_xpx(x_hist, b_255, 255);
+    [X, c1] = ret_px_xpx(x_hist, 0, b_255);
+    [X, e1] = ret_px_xpx(x_hist, 0, 255);
+    d1 = b1;
+      
+    num = (1-b) * ybar - a1 + b * b1;
+    den = c1 + b * d1 - b * e1;
+    kbar = b * (num/den);
 end
 
-function y = adapt_nonlinear_stretch(x, x255)
-    kbar = calc_kbar(x255)
+function y = adapt_nonlinear_stretch(x)
+    
+    b = otsu_threshold(x);            %%% In range 0-1
+    kbar = calc_kbar(x, b);
+    
     xmin = double(min(x(:)));
     xmax = double(max(x(:)));
-    b = otsu_threshold(x)/255.0           %%%Value of Adaptation level
+    
     mu = 0.5;  %%%% Parameter lie between [0, 1]
-    k = mu + (1 - mu)*b;   %%%% Check 
+    %mu = mean(x(:));
+    
+    k_low = (1 - mu)*b;
+    k_high = mu + (1 - mu)*b;
+
+    if kbar > k_high
+        k = k_high;
+    elseif kbar <= k_high && kbar >= k_low
+        k = kbar;
+    else
+        k = k_low;
+    end
+     
     [M,N]=size(x); % get size of image
     y = x;
     for i = 1:M
---
+++ Source/non_linear_stretching_sigmoid.m
@@ -0,0 +1,42 @@
+close all;
+clear all;
+clc
+%%================================================================================================
+D = '../Dataset/Part A/';
+% D = '../Dataset/Part B/';
+S = fullfile(pwd, D, 'IMG_1.png');
+im = imread(S); % Read the Image
+hsv_im = rgb2hsv(im);
+
+i = hsv_im(:,:,3);
+i_255 = uint8(i*255);
+
+i_new = linear_stretch(i);
+hsv_im(:,:,3) = i_new;
+y =  hsv2rgb(hsv_im);
+
+figure; imshow(im), title('Original Image');
+figure; imshow(y), title('Linear Stretched Image')
+
+figure; imhist(im)
+figure; imhist(y)
+
+brisque_orig_img = round(brisque(im), 4);
+brisque_adaptnonlinear = round(brisque(y), 4);
+
+function op_im = linear_stretch(im)
+    x = im;
+    ymin = 0;
+    ymax = 1;
+    
+    xmin = double(min(x(:)));
+    xmax = double(max(x(:)));
+    
+    y=x;
+    for i=0:255
+        I = find(x==i); %index of pixels in input image with value ‘i’
+        y(I) = (i-xmin)/(xmax-xmin);
+    end
+    
+    op_im = y;
+end
